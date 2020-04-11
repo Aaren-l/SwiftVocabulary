0b7b05a7e57989b12cc3a3cde8a9c5b80b8370e5
Aaren Lopez <aarenlopez1@gmail.com>
2020-04-10 22:02:50 -0600

    Initial Commit

---
+++ SwiftVocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftVocabulary
+//
+//  Created by Aaren Lopez on 4/10/20.
+//  Copyright © 2020 Aaren Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
