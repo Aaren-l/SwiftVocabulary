ca2dedc94f4e530e535d345e6ac996d1ee954eb3
Aaren Lopez <aarenlopez1@gmail.com>
2020-04-12 18:40:10 -0600

    Implemented DefinitionViewController and added a updateViews() methos

--- SwiftVocabulary/DefinitionViewController.swift
+++ SwiftVocabulary/DefinitionViewController.swift
@@ -9,27 +9,18 @@
 import UIKit
 
 class DefinitionViewController: UIViewController {
-    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var text: UITextView!
     var vocabWord: VocabularyWord?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateView()
     }
-    
     func updateView() {
         if let unwrappedword = vocabWord {
-            
+            label.text = unwrappedword.word
+            text.text = unwrappedword.definition
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- SwiftVocabulary/WordsTableViewController.swift
+++ SwiftVocabulary/WordsTableViewController.swift
@@ -12,7 +12,7 @@ class WordsTableViewController: UITableViewController {
     
             var vocabWords: [VocabularyWord] = [
             VocabularyWord(word: "Array", definition: "A data type that stores multiple Values of the same type on an ordered list."),
-            VocabularyWord(word: "nil", definition: "The adsence of a value or no value."),
+            VocabularyWord(word: "Nil", definition: "The adsence of a value or no value."),
             VocabularyWord(word: "Structure", definition: "A data type that’s similar to a class, but doesn’t support inheritance and is passed by value instead of by reference."),
             VocabularyWord(word: "Enumeration", definition: "A data type that defines a group of related values and enables you to work with those values in a type-safe way within your code."),
             VocabularyWord(word: "Instance", definition: "A specific occurrence of a class (that is, an object), structure, or enumeration."),
@@ -40,12 +40,8 @@ class WordsTableViewController: UITableViewController {
         if segue.identifier == "ShowDefinitonSegue" {
             if let controller = segue.destination as? DefinitionViewController,
                 let indexPath = self.tableView.indexPathForSelectedRow {
-                controller.vocabWords
+                controller.vocabWord = vocabWords[indexPath.row]
             }
         }
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    
-    
 }
